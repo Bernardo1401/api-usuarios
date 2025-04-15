819996f144614d2fc0cb48bc84c2f2a23e75e7fe
Bernardo <sardinhaber@gmail.com>
2025-04-15 15:47:01 -0300

    feat:create swagger and upload

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const cors = require("cors");
 const userRoutes = require("./src/routes/userRoutes");
 const postRoutes = require("./src/routes/postRoutes");
 const reportRoutes = require("./src/routes/reportRoutes");
+const setupSwagger = require('./src/config/swagger'); 
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+setupSwagger(app);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/users", userRoutes);
 app.use("/api/post", postRoutes);
